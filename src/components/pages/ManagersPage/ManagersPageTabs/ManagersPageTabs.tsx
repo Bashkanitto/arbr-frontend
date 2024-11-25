04228d4e25ebd00b1e43d939237a18c9ae3a28a2
Aidyn <Aidyn.meiramkhanuly@gmail.com>
2024-11-25 23:14:53 +0800

    add export pdf

--- src/components/pages/ManagersPage/ManagersPageTabs/ManagersPageTabs.tsx
+++ src/components/pages/ManagersPage/ManagersPageTabs/ManagersPageTabs.tsx
@@ -1,9 +1,53 @@
+import { format } from 'date-fns'
+import jsPDF from 'jspdf'
+import { useEffect, useState } from 'react'
+import {
+	AccountType,
+	fetchAccounts,
+} from '../../../../services/api/AccountsService'
 import { BaseButton } from '../../../atoms/Button/BaseButton'
 import { Select } from '../../../atoms/Select'
 import { Tabs } from '../../../atoms/Tabs'
 import styles from './ManagersPageTabs.module.scss'
 
 export const ManagersPageTabs = () => {
+	const [lastConfirmedAccounts, setLastConfirmedAccounts] = useState<
+		AccountType[]
+	>([])
+
+	useEffect(() => {
+		const loadLastConfirmedAccounts = async () => {
+			try {
+				const accounts = await fetchAccounts()
+				setLastConfirmedAccounts(accounts)
+			} catch (err: unknown) {
+				console.error(err)
+			}
+		}
+
+		loadLastConfirmedAccounts()
+	})
+
+	const handleExport = () => {
+		// Create a new instance of jsPDF
+		const doc = new jsPDF()
+		// Example table or data
+		doc.setFontSize(12)
+		lastConfirmedAccounts.forEach((item, index) => {
+			doc.text(
+				`${index + 1}. ${item.firstName} - ${item.role} - ${format(
+					new Date(item.createdAt),
+					'dd.MM.yy - HH:mm'
+				)}`,
+				10,
+				50 + index * 10
+			)
+		})
+
+		// Save the file
+		doc.save('managers_report.pdf')
+	}
+
 	return (
 		<div className={styles['managers-page-tabs']}>
 			<Tabs className={styles['tab']} defaultValue='all'>
@@ -27,7 +71,7 @@ export const ManagersPageTabs = () => {
 							{ value: 'inactive', label: 'Неактивные' },
 						]}
 					/>
-					<BaseButton>Экспорт</BaseButton>
+					<BaseButton onClick={handleExport}>Экспорт</BaseButton>
 				</Tabs.Panel>
 				<Tabs.Panel value='groups'>
 					<></>
--- src/components/pages/SecurityPage/SecurityPage/SecurityPageTable.tsx
+++ src/components/pages/SecurityPage/SecurityPage/SecurityPageTable.tsx
@@ -2,6 +2,7 @@
 import { Checkbox, Select } from '@mantine/core'
 import { format } from 'date-fns'
 import { ru } from 'date-fns/locale'
+import jsPDF from 'jspdf'
 import { ChangeEvent, useEffect, useState } from 'react'
 import { fetchAccounts } from '../../../../services/api/AccountsService'
 import { BaseButton } from '../../../atoms/Button/BaseButton'
@@ -13,9 +14,11 @@ import styles from './SecurityPageTable.module.scss'
 interface User {
 	firstName: string
 	id: number
+	role: string
 	customer: string
 	status: 'Создано' | 'Подтверждено'
 	updatedAt: string
+	createdAt: string
 }
 
 export const SecurityPageTable = () => {
@@ -42,9 +45,28 @@ export const SecurityPageTable = () => {
 
 		loadLastConfirmedAccounts()
 	}, [])
-
 	if (loading) return <div>Loading...</div>
 	if (error) return <div>Error: {error}</div>
+
+	const handleExport = () => {
+		// Create a new instance of jsPDF
+		const doc = new jsPDF()
+		// Example table or data
+		doc.setFontSize(12)
+		userData.forEach((item, index) => {
+			doc.text(
+				`${index + 1}. ${item.firstName} - ${item.role} - ${format(
+					new Date(item.createdAt),
+					'dd.MM.yy - HH:mm'
+				)}`,
+				10,
+				50 + index * 10
+			)
+		})
+
+		// Save the file
+		doc.save('managers_report.pdf')
+	}
 
 	const handleSelectAllChange = (event: ChangeEvent<HTMLInputElement>) => {
 		const { checked } = event.target
@@ -127,7 +149,7 @@ export const SecurityPageTable = () => {
 						value={statusFilter || ''}
 					/>
 				</div>
-				<BaseButton>Экспорт</BaseButton>
+				<BaseButton onClick={handleExport}>Экспорт</BaseButton>
 			</div>
 			<div className={styles['security-page-table']}>
 				<Table stickyHeader>
