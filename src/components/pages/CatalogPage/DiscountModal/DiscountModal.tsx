4338db29828172aedef503dc3e1b5666f413fc90
aidynM <aidyn.meiramkhanuly@gmail.com>
2024-11-29 01:40:37 +0600

    procent and discoutn works

--- src/components/pages/CatalogPage/DiscountModal/DiscountModal.tsx
+++ src/components/pages/CatalogPage/DiscountModal/DiscountModal.tsx
@@ -1,32 +1,85 @@
 import { Checkbox, Modal, Select, Slider } from '@mantine/core'
+import { useState } from 'react'
+import { updateDiscount } from '../../../../services/api/procentService'
 import { BaseButton } from '../../../atoms/Button/BaseButton'
 import styles from './DiscountModal.module.scss'
 
-interface DiscountModal {
+interface DiscountModalProps {
 	isOpen: boolean
 	onClose: () => void
+	user: any
 }
 
-const DiscountModal = ({ isOpen, onClose }: DiscountModal) => {
+const EditProcentModal = ({ isOpen, onClose, user }: DiscountModalProps) => {
+	const [sliderValue, setSliderValue] = useState<number>(0)
+	const [discount, setDiscount] = useState<number>(0)
+	const [error, setError] = useState<string>('')
+	const [selectedProductId, setSelectedProductId] = useState<number | null>(
+		null
+	)
+	const [isApplyToAll, setIsApplyToAll] = useState<boolean>(false)
+
+	const handleSave = async () => {
+		if (selectedProductId !== null) {
+			try {
+				await updateDiscount(selectedProductId, sliderValue)
+				onClose()
+			} catch (error) {
+				console.error('Error updating bonus:', error)
+			}
+		} else {
+			console.error('No product selected')
+			setError('Выберите продукт')
+		}
+	}
+
+	const handleSliderChange = (value: number) => {
+		setSliderValue(value)
+		setDiscount(value)
+	}
+
+	const handleSelectChange = (value: string | null) => {
+		if (value !== null) {
+			setError('')
+			setSelectedProductId(Number(value))
+		} else {
+			setSelectedProductId(null)
+		}
+	}
+
 	return (
 		<Modal
-			className={styles['DiscountModal-modal']}
+			className={styles['Discount-modal']}
 			opened={isOpen}
 			onClose={onClose}
 		>
 			<Select
 				placeholder='Выберите товар'
-				data={[
-					{ value: 'Перчатки', label: 'Перчатки' },
-					{ value: 'Белизна', label: 'Белизна' },
-				]}
+				onChange={handleSelectChange}
+				data={user.vendorGroups.map((group: any) => ({
+					value: String(group.product.id),
+					label: group.product.name,
+				}))}
 			/>
+			{error && <p className='danger'>{error}</p>}
 			<div className={styles['checkbox']}>
-				<Checkbox size='xs' /> Применить ко всем товарам
+				<Checkbox
+					size='xs'
+					checked={isApplyToAll}
+					onChange={e => setIsApplyToAll(e.target.checked)}
+				/>
+				Применить ко всем товарам
 			</div>
-			<p className={styles['procent']}>Укажите процент скидки</p>
-			<Slider color='blue' />
+			<Slider
+				color='blue'
+				value={sliderValue}
+				onChange={handleSliderChange}
+				min={0}
+				max={100}
+			/>
+			<p className={styles.bonus}>Скидка {discount}%</p>
 			<BaseButton
+				onClick={handleSave}
 				className={styles['DiscountModal-button']}
 				variantColor='primary'
 			>
@@ -36,4 +89,4 @@ const DiscountModal = ({ isOpen, onClose }: DiscountModal) => {
 	)
 }
 
-export default DiscountModal
+export default EditProcentModal
--- src/services/api/procentService.ts
+++ src/services/api/procentService.ts
@@ -1,24 +1,42 @@
-// src/services/api/procentService.ts
 import baseApi from './base'
 
 export const updateBonus = async (productId: number, bonus: number) => {
-	const updatedData = {
-		features: {
-			isBonus: true,
-			isFreeDelivery: true,
-			isDiscount: true,
-			bonus,
-			discount: 0,
-		},
-	}
-
 	try {
+		const updatedData = {
+			features: {
+				isBonus: true,
+				bonus,
+			},
+		}
+
 		const response = await baseApi.patch(
 			`/vendor-group/${productId}`,
 			updatedData
 		)
 		return response.data
 	} catch (error) {
+		console.error('Error updating bonus:', error)
 		throw new Error('Error updating bonus')
+	}
+}
+
+// ––––––––––––––––––––––––––––Discount–––––––––––––––––––––––––––––––
+export const updateDiscount = async (productId: number, discount: number) => {
+	try {
+		const updatedData = {
+			features: {
+				isDiscount: true,
+				discount,
+			},
+		}
+
+		const response = await baseApi.patch(
+			`/vendor-group/${productId}`,
+			updatedData
+		)
+		return response.data
+	} catch (error) {
+		console.error('Error updating discount:', error)
+		throw new Error('Error updating discount')
 	}
 }
