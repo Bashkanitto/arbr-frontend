eacd16ce4b5102f41e6592a2a37feab68926504c
aidynM <aidyn.meiramkhanuly@gmail.com>
2024-12-05 18:30:10 +0500

    add download btn for product page

--- src/components/pages/ProductPage/ProductPage.tsx
+++ src/components/pages/ProductPage/ProductPage.tsx
@@ -1,6 +1,9 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Skeleton } from '@mantine/core'
+import jsPDF from 'jspdf'
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import '../../../assets/fonts/Roboto-Regular.ttf'
+import { DownloadIcon } from '../../../assets/icons/DownloadIcon'
 import { fetchProductById } from '../../../services/api/productService'
 import styles from './ProductPage.module.scss'
 
@@ -8,9 +11,9 @@ interface Product {
 	name: string
 	quantity: number
 	price: number
+	description: string
 	images: { url: string }[]
 	order: string
-	details: string
 }
 
 const ProductPage = () => {
@@ -18,8 +21,8 @@ const ProductPage = () => {
 	const [product, setProduct] = useState<Product | null>(null)
 	const [loading, setLoading] = useState<boolean>(true)
 	const [error, setError] = useState<string | null>(null)
+	const [activeTab, setActiveTab] = useState<string>('описание')
 
-	// Fetch product details on component mount
 	useEffect(() => {
 		const loadProduct = async () => {
 			try {
@@ -39,11 +42,27 @@ const ProductPage = () => {
 		loadProduct()
 	}, [id])
 
-	// Handling loading and error states
-	if (loading) return <div>Loading...</div>
+	if (loading) return <Skeleton />
 	if (error) return <div>{error}</div>
-
 	if (!product) return <div>Product not found.</div>
+
+	const handleTabClick = (tab: string) => {
+		setActiveTab(tab)
+	}
+
+	const downloadPDF = () => {
+		const doc = new jsPDF()
+		doc.addFont('Roboto-Regular.ttf', 'Roboto', 'normal')
+		doc.setFont('Roboto')
+		doc.setFontSize(16)
+		doc.text('Product Details', 10, 10)
+		doc.setFontSize(12)
+		doc.text(`Name: ${product.name}`, 10, 20)
+		doc.text(`Price: ${product.price}₸`, 10, 30)
+		doc.text(`Quantity: ${product.quantity} шт`, 10, 40)
+		doc.text(`Description ${product.description}:`, 10, 50)
+		doc.save(`${product.name}_details.pdf`)
+	}
 
 	return (
 		<div className={styles['product-page']}>
@@ -51,6 +70,42 @@ const ProductPage = () => {
 				<img src={product.images[0]?.url} alt='' />
 				<img src={product.images[1]?.url} alt='' />
 				<img src={product.images[2]?.url} alt='' />
+				<div className={styles.tabs}>
+					<ul>
+						<li
+							className={activeTab === 'описание' ? styles.active : ''}
+							onClick={() => handleTabClick('описание')}
+						>
+							Описание
+						</li>
+						<li
+							className={activeTab === 'Характеристики' ? styles.active : ''}
+							onClick={() => handleTabClick('Характеристики')}
+						>
+							Характеристики
+						</li>
+						<li
+							className={activeTab === 'Документы' ? styles.active : ''}
+							onClick={() => handleTabClick('Документы')}
+						>
+							Документы
+						</li>
+					</ul>
+
+					{activeTab === 'описание' && (
+						<div id='description' className={styles.tabBody}>
+							{product.description}
+						</div>
+					)}
+					{activeTab === 'Документы' && (
+						<div className={styles.tabBody}>
+							<button className={styles.downloadBtn} onClick={downloadPDF}>
+								<DownloadIcon />
+								Скачать документ
+							</button>
+						</div>
+					)}
+				</div>
 			</div>
 			<div className={styles['product-info']}>
 				<div className={styles['container']}>
@@ -67,15 +122,11 @@ const ProductPage = () => {
 					</div>
 					<button>% ИЗМЕНИТЬ ПРОЦЕНТ</button>
 					<div className={styles['product-details']}>
-						<p>
+						<a href='#description'>
 							Доставка <span>{product.order}</span>
-						</p>
-						<p>
-							Описание <span>K</span>
-						</p>
-						<p>
-							Характеристики <span>{product.details}</span>
-						</p>
+						</a>
+						<a href='#description'>Описание</a>
+						<a href='#description'>Характеристики</a>
 					</div>
 				</div>
 			</div>
