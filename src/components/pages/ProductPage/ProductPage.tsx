0622391ab3dc74d6530313ffacd21ef1c5ead148
aidynM <aidyn.meiramkhanuly@gmail.com>
2025-01-14 02:08:47 +0500

    fix productpage

--- src/components/pages/ProductPage/ProductPage.tsx
+++ src/components/pages/ProductPage/ProductPage.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Skeleton, TextInput } from '@mantine/core'
 import jsPDF from 'jspdf'
 import { useEffect, useState } from 'react'
@@ -11,24 +12,19 @@ import EditProcentModal from '../CatalogPage/EditProcentModal/EditProcentModal'
 import NotFoundPage from '../NotFoundPage/NotFoundPage'
 import styles from './ProductPage.module.scss'
 import { BaseButton } from '../../atoms/Button/BaseButton'
-
-interface Product {
-	name: string
-	id: number
-	quantity: number
-	price: number
-	description: string
-	images: { url: string }[]
-	order: string
-}
+import { ProductType } from '../../../services/api/Types'
+import FullViewImageModal from '../../molecules/FullViewImageModal/FullViewImageModal'
 
 const ProductPage = () => {
 	const { id } = useParams<{ id: string }>()
-	const [vendorGroup, setVendorGroup] = useState<Product | null>(null)
+	const [vendorGroup, setVendorGroup] = useState<any | null>(null)
 	const [loading, setLoading] = useState<boolean>(true)
+	const [infoVisibility, setInfoVisibility] = useState<number | null>()
 	const [error, setError] = useState<string | null>(null)
 	const [activeTab, setActiveTab] = useState<string>('описание')
 	const [isEditModalOpen, setIsEditModalOpen] = useState<boolean>(false)
+	const [isViewModalOpen, setIsViewModalOpen] = useState<boolean>(false)
+	const [selectedImage, setSelectedImage] = useState<string>('')
 
 	const [selectedFiles, setSelectedFiles] = useState<File[]>([])
 
@@ -37,6 +33,16 @@ const ProductPage = () => {
 			setSelectedFiles(Array.from(event.target.files))
 		}
 	}
+	function openViewModal(imageUrl: string) {
+		setSelectedImage(imageUrl)
+		setIsViewModalOpen(true)
+	}
+
+	const closeViewModal = () => {
+		setIsViewModalOpen(false)
+		setSelectedImage('')
+	}
+
 	const vendorGroupId = vendorGroup ? vendorGroup.id : null
 
 	const handleSubmit = async () => {
@@ -69,13 +75,12 @@ const ProductPage = () => {
 		loadProduct()
 	}, [id])
 
-	if (loading) return <Skeleton />
 	if (error) return <div>{error}</div>
 	if (!vendorGroup) {
 		return <NotFoundPage />
 	}
 
-	const { product }: any = vendorGroup
+	const product: ProductType = vendorGroup.product
 
 	const getTextColor = (quantity: number): string => {
 		if (quantity < 20) return 'danger' // красный цвет
@@ -103,16 +108,25 @@ const ProductPage = () => {
 		doc.text(`Name: ${product.name}`, 10, 20)
 		doc.text(`Price: ${product.price}₸`, 10, 30)
 		doc.text(`Quantity: ${product.quantity} шт`, 10, 40)
-		doc.text(`Description ${product.description}:`, 10, 50)
+		doc.text(`Description ${product.options}:`, 10, 50)
 		doc.save(`${product.name}_details.pdf`)
 	}
 
 	return (
 		<div className={styles['product-page']}>
 			<div className={styles['product-image']}>
-				{product.images?.map((image: { url: string }) => (
-					<img src={image.url} alt='' />
-				))}
+				{product.images?.map((image: { url: string }) =>
+					loading ? (
+						<Skeleton width={600} height={400} radius={15} />
+					) : (
+						<img
+							onClick={() => openViewModal(image.url)}
+							style={{ cursor: 'pointer' }}
+							src={image.url}
+							alt=''
+						/>
+					)
+				)}
 				<div className={styles.tabs}>
 					<ul>
 						<li
@@ -121,12 +135,7 @@ const ProductPage = () => {
 						>
 							Описание
 						</li>
-						<li
-							className={activeTab === 'Характеристики' ? styles.active : ''}
-							onClick={() => handleTabClick('Характеристики')}
-						>
-							Характеристики
-						</li>
+
 						<li
 							className={activeTab === 'Документы' ? styles.active : ''}
 							onClick={() => handleTabClick('Документы')}
@@ -137,7 +146,7 @@ const ProductPage = () => {
 
 					{activeTab === 'описание' && (
 						<div id='description' className={styles.tabBody}>
-							{product.description}
+							{product.options}
 						</div>
 					)}
 					{activeTab === 'Документы' && (
@@ -173,14 +182,37 @@ const ProductPage = () => {
 					</div>
 					<button onClick={openEditModal}>% ИЗМЕНИТЬ ПРОЦЕНТ</button>
 					<div className={styles['product-details']}>
-						<a href='#description'>
-							Доставка <span>{product.order}</span>
+						<a onClick={() => setInfoVisibility(0)}>
+							Характеристики
+							<p style={{ height: infoVisibility == 0 ? '100px' : '0px' }}>
+								{product.options}
+							</p>
 						</a>
-						<a href='#description'>Характеристики</a>
-						<a href='#description'>Описание</a>
-						<a href='#description'>ЕНС ТРУ</a>
-						<a href='#description'>GTIN (штрихкод)</a>
-						<a href='#description'>KZTIN (для гос. закупок)</a>
+						<a onClick={() => setInfoVisibility(1)}>
+							ЕНС ТРУ
+							<p style={{ height: infoVisibility == 1 ? '40px' : '0px' }}>
+								{product.ENSTRU}
+							</p>
+						</a>
+						<a onClick={() => setInfoVisibility(2)}>
+							GTIN (штрихкод)
+							<p style={{ height: infoVisibility == 2 ? '40px' : '0px' }}>
+								{product.code}
+							</p>
+						</a>
+						<a onClick={() => setInfoVisibility(3)}>
+							KZTIN (для гос. закупок)
+							<p style={{ height: infoVisibility == 3 ? '40px' : '0px' }}>
+								{product.KZTIN}
+							</p>
+						</a>
+
+						<a onClick={() => setInfoVisibility(4)}>
+							Статус
+							<p style={{ height: infoVisibility == 4 ? '40px' : '0px' }}>
+								{product.status}
+							</p>
+						</a>
 					</div>
 				</div>
 			</div>
@@ -189,6 +221,12 @@ const ProductPage = () => {
 				isOpen={isEditModalOpen}
 				onClose={closeEditModal}
 				user={{ vendorGroups: [{ product }] }}
+			/>
+
+			<FullViewImageModal
+				imageUrl={selectedImage}
+				isOpen={isViewModalOpen}
+				onClose={closeViewModal}
 			/>
 		</div>
 	)
