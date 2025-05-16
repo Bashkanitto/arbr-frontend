f21cb6c37cc8de5f622c5538d8a78aa59b17e5e7
aidyn <aidyn.meiramkhanuly@gmail.com>
2025-05-17 03:26:03 +0500

    fix: cover with providers, add ci/cd for test, add vitest script, add beforeAll setting for matchMedia

--- src/features/Auth/LoginForm.test.tsx
+++ src/features/Auth/LoginForm.test.tsx
@@ -1,12 +1,20 @@
+import { MantineProvider } from '@mantine/core'
 import AuthPage from '@pages/AuthPage/AuthPage'
 import { render, screen } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
 import { describe, it, expect } from 'vitest'
 
 describe('LoginForm', () => {
   it('renders input and button', () => {
-    render(<AuthPage />)
+    render(
+      <MemoryRouter>
+        <MantineProvider>
+          <AuthPage />
+        </MantineProvider>
+      </MemoryRouter>
+    )
     expect(screen.getByPlaceholderText('Ваша Почта')).toBeInTheDocument()
     expect(screen.getByPlaceholderText('Ваш Пароль')).toBeInTheDocument()
-    expect(screen.getByRole('button', { name: 'Login' })).toBeInTheDocument()
+    expect(screen.getByRole('button', { name: 'Войти' })).toBeInTheDocument()
   })
 })
--- src/shared/test/setup.ts
+++ src/shared/test/setup.ts
@@ -1,1 +1,19 @@
 import '@testing-library/jest-dom'
+import { beforeAll } from 'vitest'
+
+beforeAll(() => {
+  window.matchMedia =
+    window.matchMedia ||
+    function () {
+      return {
+        matches: false,
+        media: '',
+        onchange: null,
+        addListener: () => {}, // deprecated
+        removeListener: () => {}, // deprecated
+        addEventListener: () => {},
+        removeEventListener: () => {},
+        dispatchEvent: () => false,
+      }
+    }
+})
