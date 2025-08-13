87d15dffdc799f349cecf5ce981e5fa867b9d148
aidyn <aidyn.meiramkhanuly@gmail.com>
2025-08-13 20:41:44 +0500

    fix styles

--- src/pages/ApplicationPage/ApplicationTabble/ApplicationTabbleAdmin.tsx
+++ src/pages/ApplicationPage/ApplicationTabble/ApplicationTabbleAdmin.tsx
@@ -122,7 +122,7 @@ export const ApplicationTableAdmin = () => {
             }}
           >
             <button
-              className={styles.statusNotBt}
+              className={styles.statusNotBtn}
               onClick={() => handleStatusChange(item.product?.id, 'inactive')}
             >
               <img src="/images/diskLike_photo.svg" alt="" />
